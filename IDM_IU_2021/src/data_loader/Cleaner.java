e6481f0ad946b29ab7f64e3fd7fc08559672e495
Tran Duc Tri <tranductrilxag@gmail.com>
2021-04-20 19:22:44 +0700

    update Cleaner for CustomerId values

--- IDM_IU_2021/src/data_loader/Cleaner.java
+++ IDM_IU_2021/src/data_loader/Cleaner.java
@@ -1,8 +1,13 @@
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.converters.Loader;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Cleaner {
-    public Instances cleanData(Instances data) {
+    public static Instances cleanData(Instances data) {
         int indexInvoiceNo = data.attribute("InvoiceNo").index();
         int indexQuantity = data.attribute("Quantity").index();
         int indexUnitPrice = data.attribute("UnitPrice").index();
@@ -13,10 +18,18 @@ public class Cleaner {
             if (inst.toString(indexInvoiceNo) == "C"
                     || Integer.valueOf(inst.toString(indexQuantity)) < 0
                     || Double.valueOf(inst.toString(indexUnitPrice)) < 0
-                    || inst.toString(indexCustomerID).equals("NaN")) {
+                    || inst.toString(indexCustomerID).equals("?")) {
                 data.delete(i);
             }
         }
         return data;
     }
+//    public static void main(String[] args) {
+//        try {
+//            DataSource dts = new DataSource("C:\\Users\\DELL\\Desktop\\data.arff");
+//            Instances data = dts.getDataSet();
+//            System.out.println(cleanData(data));
+//        } catch(Exception e)
+//        { e.printStackTrace();}
+//        }
 }
