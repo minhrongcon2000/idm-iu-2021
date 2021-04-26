5c9c87a1cb118846bfc390bfdf9c2e2349c374d5
barbatoz0220 <47182649+barbatoz0220@users.noreply.github.com>
2021-04-26 10:53:33 +0700

    Wrote function to create .arff file in RFM.java

--- IDM_IU_2021/src/data/RFM.java
+++ IDM_IU_2021/src/data/RFM.java
@@ -1,11 +1,9 @@
 package data;
 
-import weka.core.Attribute;
-import weka.core.Instance;
-import weka.core.Instances;
+import weka.core.*;
 import weka.core.converters.ConverterUtils.DataSource;
-
-import java.text.ParseException;
+import weka.core.converters.ConverterUtils.DataSink;
+import weka.core.Instances;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import org.javatuples.*;
@@ -25,7 +23,8 @@ public class RFM {
     private final ArrayList<Integer> monetaryList = new ArrayList<>();
     private final HashMap<String, Quintet<Date, Integer, Integer, Double, Integer>> invoiceMap = new HashMap<>();
     private final HashMap<Integer, Pair<String, ArrayList<Integer>>> customerValues = new HashMap<>();
-    private final HashMap<Integer, ArrayList<Integer>> RFMTable = new HashMap<>();
+    private final HashMap<Integer, ArrayList<Integer>> RFMMap = new HashMap<>();
+    private final Instances RFMTable = new Instances("RFMTable", new ArrayList<>(), RFMMap.size());
 
 
     // Supporting functions
@@ -47,8 +46,8 @@ public class RFM {
         score = a + ((curValue - minValue)*(b-a))/(maxValue-minValue);
         return score;
     }
-    public HashMap<Integer, ArrayList<Integer>> getRFMTable() {
-        return this.RFMTable;
+    public HashMap<Integer, ArrayList<Integer>> getRFMMap() {
+        return this.RFMMap;
     }
 
     // Main functions
@@ -90,13 +89,13 @@ public class RFM {
                 int oldDate = customerValues.get(customerId).getValue1().get(0);
                 int currentNoOfInvoice = customerValues.get(customerId).getValue1().get(1) + 1;
                 int oldPrice = customerValues.get(customerId).getValue1().get(2);
-                // Modify minDate of customer
+                // Modify MinDate of customer
                 if (invoiceDate < oldDate)
                     customerValues.get(customerId).getValue1().set(0, invoiceDate);
                 // Modify noOfInvoices
                 customerValues.get(customerId).getValue1().set(1, Math.min(currentNoOfInvoice, 10));
                 // Modify MonetaryValues
-                if (invoicePrice > oldPrice)
+                if (invoicePrice > oldPrice && invoicePrice != 0)
                     customerValues.get(customerId).getValue1().set(2, (int) invoicePrice);
             } else {
                 customerValues.put(customerId, new Pair<>(invoiceNo, new ArrayList<>()));
@@ -107,33 +106,68 @@ public class RFM {
         }
     }
     public Integer[] generateRFMLists() {
+        // int c1 = 0, c2 = 0, c3 = 0, c4 = 0;
         for (int customerId : customerValues.keySet()) {
             recencyList.add(customerValues.get(customerId).getValue1().get(0));
             frequencyList.add(customerValues.get(customerId).getValue1().get(1));
+            /* Counter
+            if (customerValues.get(customerId).getValue1().get(2) < 100)
+                c1 += 1;
+            else if (customerValues.get(customerId).getValue1().get(2) >= 100 && customerValues.get(customerId).getValue1().get(2) < 1000)
+                c2 += 1;
+            else if (customerValues.get(customerId).getValue1().get(2) >= 1000 && customerValues.get(customerId).getValue1().get(2) < 10000)
+                c3 += 1;
+            else
+                c4 += 1;
+             */
             monetaryList.add(customerValues.get(customerId).getValue1().get(2));
         }
-        int maxDate = 0, maxPrice = 0;
+        int maxDate = 0, maxPrice = 0, minPrice = monetaryList.get(0);
         for (int i = 0; i < monetaryList.size(); i++) {
-            if (recencyList.get(i) > maxDate)
+            if (recencyList.get(i) >= maxDate)
                 maxDate = recencyList.get(i);
-            if (monetaryList.get(i) > maxPrice)
+            if (monetaryList.get(i) >= maxPrice)
                 maxPrice = monetaryList.get(i);
+            if (monetaryList.get(i) <= minPrice && monetaryList.get(i) != 0)
+                minPrice = monetaryList.get(i);
         }
-        Integer[] maxValues = new Integer[2];
+        Integer[] maxValues = new Integer[3];
         maxValues[0] = maxDate;
         maxValues[1] = maxPrice;
+        maxValues[2] = minPrice;
         return maxValues;
     }
-    public void generateRFMTable(Integer[] maxValues) {
-        int maxDate = maxValues[0], maxPrice = maxValues[1];
+    public void generateRFMMap(Integer[] extremeValues) {
+        int maxDate = extremeValues[0], maxPrice = extremeValues[1], minPrice = extremeValues[2];
         for (int customerId : customerValues.keySet()) {
             int RScore = minMaxScaling(customerValues.get(customerId).getValue1().get(0), maxDate, 0);
-            int FScore = minMaxScaling(customerValues.get(customerId).getValue1().get(1), 10, 0);
-            int MScore = minMaxScaling(customerValues.get(customerId).getValue1().get(2), maxPrice, 0);
-            RFMTable.put(customerId, new ArrayList<>());
-            RFMTable.get(customerId).add(RScore);
-            RFMTable.get(customerId).add(FScore);
-            RFMTable.get(customerId).add(MScore);
+            int FScore = minMaxScaling(customerValues.get(customerId).getValue1().get(1), 10, 1);
+            int MScore = minMaxScaling(customerValues.get(customerId).getValue1().get(2), maxPrice, minPrice);
+            RFMMap.put(customerId, new ArrayList<>());
+            RFMMap.get(customerId).add(RScore);
+            RFMMap.get(customerId).add(FScore);
+            RFMMap.get(customerId).add(MScore);
+        }
+    }
+    public void createRFMInstance() {
+        RFMTable.insertAttributeAt(new Attribute("CustomerID", false), 0);
+        RFMTable.insertAttributeAt(new Attribute("Recency"), 1);
+        RFMTable.insertAttributeAt(new Attribute("Frequency"), 2);
+        RFMTable.insertAttributeAt(new Attribute("Monetary"), 3);
+        for (Integer customerId : RFMMap.keySet()) {
+            DenseInstance customerInstance = new DenseInstance(4);
+            customerInstance.setValue(0, customerId);
+            customerInstance.setValue(1, RFMMap.get(customerId).get(0));
+            customerInstance.setValue(2, RFMMap.get(customerId).get(1));
+            customerInstance.setValue(3, RFMMap.get(customerId).get(2));
+            RFMTable.add(customerInstance);
+        }
+        String outputFile = "D:\\rfmTable.arff";
+        try {
+            DataSink.write(outputFile,RFMTable);
+        } catch(Exception e) {
+            System.out.println("Couldn't write data to " + outputFile);
+            e.printStackTrace();
         }
     }
 
@@ -141,12 +175,12 @@ public class RFM {
     public RFM() throws Exception {
         generateInvoiceMap();
         generateCustomerValues();
-        Integer[] maxValues = generateRFMLists();
-        generateRFMTable(maxValues);
+        Integer[] extremeValues = generateRFMLists();
+        generateRFMMap(extremeValues);
     }
 
     public static void main(String[] args) throws Exception {
         RFM test = new RFM();
-        HashMap<Integer, ArrayList<Integer>> RFMTable = test.getRFMTable();
+        test.createRFMInstance();
     }
 }
