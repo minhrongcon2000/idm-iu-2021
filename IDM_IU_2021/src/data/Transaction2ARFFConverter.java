bb7d184a0fd486dd2fba264281f4a3984160dea0
Tran Duc Tri <tranductrilxag@gmail.com>
2021-05-02 15:07:19 +0700

    add transaction to arff converter

---
+++ IDM_IU_2021/src/data/Transaction2ARFFConverter.java
@@ -0,0 +1,65 @@
+package data;
+
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.*;
+import weka.filters.unsupervised.instance.Denormalize;
+
+// test
+import data.DataCleaner;
+import weka.core.Instance;
+import weka.core.converters.ArffLoader;
+import java.io.File;
+import java.io.IOException;
+
+public class Transaction2ARFFConverter {
+    private static Instances removeRedundantAttributes(Instances data) throws Exception {
+        Remove remove = new Remove();
+        String[] options = new String[2];
+        options[0] = "-R";
+        options[1] = "3-8";
+        remove.setOptions(options);
+        remove.setInputFormat(data);
+        Instances newData = Filter.useFilter(data, remove);
+        return newData;
+    }
+
+    private static Instances stockCodeStringToNominal(Instances data) throws Exception {
+        StringToNominal stn = new StringToNominal();
+        String[] options = new String[2];
+        options[0] = "-R";
+        options[1] = "1-2";
+        stn.setOptions(options);
+        stn.setInputFormat(data);
+        Instances newData = Filter.useFilter(data, stn);
+        return newData;
+    }
+
+    public static Instances convert(Instances inputData) throws Exception {
+        Instances data = stockCodeStringToNominal(removeRedundantAttributes(inputData));
+        Denormalize denormalize = new Denormalize();
+        String[] options = new String[2];
+        options[0] = "-G";
+        options[1] = "1";
+        denormalize.setOptions(options);
+        denormalize.setInputFormat(data);
+        Instances newData = Filter.useFilter(data, denormalize);
+        return newData;
+    }
+
+    // test
+    public static void main(String[] args) throws Exception {
+        ArffLoader loader = new ArffLoader();
+        loader.setSource(new File("T:\\1\\2-3\\Intro to Data Mining\\Project\\idm-iu-2021\\IDM_IU_2021\\data\\data.arff"));
+        Instances data = loader.getDataSet();
+        DataCleaner cleaner = new DataCleaner(data)
+                .removeCTransaction()
+                .removeInvalidPrice()
+                .removeMissingID()
+                .removeNegativeQuantity();
+        Transaction2ARFFConverter converter = new Transaction2ARFFConverter();
+        Instances newData = converter.convert(cleaner.getData());
+        System.out.println(newData);
+    }
+}
+
