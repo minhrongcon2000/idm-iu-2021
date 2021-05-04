aefca1ac4e5115c0453697ede5b1a7c98f56b1f0
Tran Duc Tri <tranductrilxag@gmail.com>
2021-05-04 23:19:39 +0700

    modify Transaction2ARFFConverter.java, export arff result file

--- IDM_IU_2021/src/data/Transaction2ARFFConverter.java
+++ IDM_IU_2021/src/data/Transaction2ARFFConverter.java
@@ -3,16 +3,24 @@ package data;
 import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.*;
-import weka.filters.unsupervised.instance.Denormalize;
-
+import weka.filters.unsupervised.instance.*;
+import weka.core.converters.ConverterUtils.DataSink;
 // test
-import data.DataCleaner;
 import weka.core.Instance;
 import weka.core.converters.ArffLoader;
 import java.io.File;
 import java.io.IOException;
 
-public class Transaction2ARFFConverter {
+public class Transaction2ARFFConverter implements iConverter {
+
+    private final String sourcePath;
+    private final String desPath;
+
+    public Transaction2ARFFConverter(String sourcePath, String desPath) {
+        this.sourcePath = sourcePath;
+        this.desPath = desPath;
+    }
+
     private static Instances removeRedundantAttributes(Instances data) throws Exception {
         Remove remove = new Remove();
         String[] options = new String[2];
@@ -35,7 +43,7 @@ public class Transaction2ARFFConverter {
         return newData;
     }
 
-    public static Instances convert(Instances inputData) throws Exception {
+    public static Instances denormalize(Instances inputData) throws Exception {
         Instances data = stockCodeStringToNominal(removeRedundantAttributes(inputData));
         Denormalize denormalize = new Denormalize();
         String[] options = new String[2];
@@ -47,19 +55,29 @@ public class Transaction2ARFFConverter {
         return newData;
     }
 
-    // test
-    public static void main(String[] args) throws Exception {
+    @Override
+    public void convert() throws Exception {
         ArffLoader loader = new ArffLoader();
-        loader.setSource(new File("T:\\1\\2-3\\Intro to Data Mining\\Project\\idm-iu-2021\\IDM_IU_2021\\data\\data.arff"));
+        loader.setSource(new File(this.sourcePath));
         Instances data = loader.getDataSet();
         DataCleaner cleaner = new DataCleaner(data)
                 .removeCTransaction()
                 .removeInvalidPrice()
                 .removeMissingID()
                 .removeNegativeQuantity();
-        Transaction2ARFFConverter converter = new Transaction2ARFFConverter();
-        Instances newData = converter.convert(cleaner.getData());
-        System.out.println(newData);
+        Instances newData = denormalize(cleaner.getData());
+        DataSink.write(this.desPath, newData);
     }
+
+    // test
+    public static void main(String[] args) throws Exception {
+        String src = "T:\\1\\2-3\\Intro to Data Mining\\Local Project\\idm-iu-2021\\IDM_IU_2021\\data\\data.arff";
+        String dest = "T:\\1\\2-3\\Intro to Data Mining\\Local Project\\idm-iu-2021\\IDM_IU_2021\\data\\transaction.arff";
+
+        Transaction2ARFFConverter converter = new Transaction2ARFFConverter(src, dest);
+        converter.convert();
+    }
+
+
 }
 
