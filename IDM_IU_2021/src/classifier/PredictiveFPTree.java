63e38f91232bff6bc4c7ffba754f900c21986a08
John Pham <minhrongcon2000@gmail.com>
2021-05-18 00:29:54 +0700

    Retrieve safe file

--- IDM_IU_2021/src/classifier/PredictiveFPTree.java
+++ IDM_IU_2021/src/classifier/PredictiveFPTree.java
@@ -1,28 +1,26 @@
 package classifier;
 
-import arm.fpgrowth.FPTree;
-import arm.IConditionalItem;
-import arm.IFPTree;
-import arm.IRule;
+import weka.associations.AssociationRule;
+import weka.associations.AssociationRules;
+import weka.associations.FPGrowth;
+import weka.associations.Item;
 import weka.classifiers.AbstractClassifier;
 import weka.classifiers.rules.ZeroR;
 import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
 
-import java.util.List;
-
 public class PredictiveFPTree extends AbstractClassifier {
-    private final FPTree tree;
+    private final FPGrowth tree;
     private final ZeroR zeroRule;
     private Attribute classAttribute;
 
     public PredictiveFPTree() {
-        tree = new FPTree();
+        tree = new FPGrowth();
         zeroRule = new ZeroR();
     }
 
-    private static int getAttributeIndex(Instance data, String attributeName) {
+    public static int getAttributeIndex(Instance data, String attributeName) {
         for (int i=0; i<data.numAttributes(); ++i) {
             if (data.attribute(i).name().equals(attributeName))
                 return i;
@@ -30,22 +28,21 @@ public class PredictiveFPTree extends AbstractClassifier {
         return -1;
     }
 
-    private double[] getProb(IFPTree tree, Instance data) {
-        List<IRule> rules = tree.getAssociationRules();
+    private double[] getProb(FPGrowth tree, Instance data) {
+        AssociationRules rules = tree.getAssociationRules();
         double[] c = {0, 0};
-        for (IRule rule: rules) {
-            List<IConditionalItem> consequences = rule.getConsequences();
-            if ((consequences.size() == 1) && (consequences.get(0).getItemName().equals(this.classAttribute.name()))) {
-                List<IConditionalItem> premise = rule.getPremise();
+        for (AssociationRule rule: rules.getRules()) {
+            Item[] consequence = rule.getConsequence().toArray(new Item[0]);
+            if ((consequence.length == 1) && (consequence[0].getAttribute().name().equals(classAttribute.name()))) {
+                Item[] antecedent = rule.getPremise().toArray(new Item[0]);
                 int s = 0;
-                for (IConditionalItem item: premise) {
-                    if (data.stringValue(getAttributeIndex(data, item.getItemName())).equals(item.getItemValue())) {
-                        ++s;
-                    }
+                for (Item item: antecedent) {
+                    if (data.stringValue(getAttributeIndex(data, item.getAttribute().name())).equals(item.getItemValueAsString()))
+                        s++;
                 }
-                if (s == premise.size()) {
-                    if (consequences.get(0).getItemValue().equals(this.classAttribute.value(0))) ++c[0];
-                    else ++c[1];
+                if (s == antecedent.length) {
+                    if (consequence[0].getItemValueAsString().equals(classAttribute.value(0))) c[0]++;
+                    else c[1]++;
                 }
             }
         }
@@ -60,7 +57,7 @@ public class PredictiveFPTree extends AbstractClassifier {
     @Override
     public void buildClassifier(Instances instances) throws Exception {
         int classIdx = instances.classIndex();
-        this.classAttribute = instances.classAttribute();
+        classAttribute = instances.classAttribute();
 
         // remove class index in order to build FP tree
         instances.setClassIndex(-1);
@@ -72,9 +69,11 @@ public class PredictiveFPTree extends AbstractClassifier {
     }
 
     @Override
-    public double classifyInstance(Instance instance) throws Exception {
+    public double classifyInstance(Instance instance) {
         double[] distribution = this.getProb(tree, instance);
-        if (distribution[0] == distribution[1]) return zeroRule.classifyInstance(instance);
+        if (distribution[0] == distribution[1]) {
+            return zeroRule.classifyInstance(instance);
+        }
         if (distribution[0] > distribution[1]) return 0.0d;
         return 1.0d;
     }
@@ -86,5 +85,9 @@ public class PredictiveFPTree extends AbstractClassifier {
             return zeroRule.distributionForInstance(instance);
         }
         return distribution;
+    }
+
+    public void setMinMetrics(double minMetrics) {
+        tree.setMinMetric(minMetrics);
     }
 }
